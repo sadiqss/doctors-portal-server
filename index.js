bcc1a66e5a8c28bfd804cd19f6680978d5588249
sadiqss <sadiq.sau48@gmail.com>
2022-05-17 15:05:49 +0100

    booking db

--- index.js
+++ index.js
@@ -19,12 +19,32 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('bookings');
+
 
         app.get('/service', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        /**
+         * API naming convention
+         * app.get('/booking') //get all bookings in this collection or get more than one
+         * app.get('/booking:id') //get a specific booking
+         * app.post('/booking') //add a new booking
+         * app.patch('/booking:id')
+         * app.delete('/booking:id') 
+         * 
+         * 
+         */
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
         })
 
     }
